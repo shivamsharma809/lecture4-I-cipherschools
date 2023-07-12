81cb061dc266adbc3cf8ef6b3566d50d4741c538
shivamsharma809 <138153296+shivamsharma809@users.noreply.github.com>
2023-07-12 12:02:36 +0530

    vs code

---
+++ Conditional.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    
+    cout<<"input 4 integers a,b,c and d"<<endl;
+
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+
+    //statement : a>b
+
+    cout<<(a>b && c>d);
+
+    // c++ interprets true as 1 
+    // c++ interprets false as 0
+
+}
